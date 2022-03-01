6a5fee5eb2fa4e3672fc26bd09477f5ac83f977d
Vitor Alves <vitormanoel00710@gmail.com>
2022-03-01 13:30:14 -0300

    :rocket: Add server nodejs with core modules full commit

---
+++ 3_CORE_MODULES_NODE/9_renomear_arquivos/index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const arqOld = "arquivo.txt";
+const arqNew = "novoarquivo.txt"
+
+fs.rename(arqOld, arqNew, function (err) {
+    if(err) {
+        console.log(err);
+
+        return;
+    }
+
+    console.log(`O arquivo ${arqOld} foi renomeado para ${arqNew}`);
+})
